9e734ca647c4eda8c45ee719ffcce6f5ca335444
Viktor Lindblad <Viktor Lindblad@DESKTOP-NK4S0QF>
2018-10-22 20:42:03 +0200

    Added graphic classes

---
+++ The Game/src/com/tutorial/main/Assets.java
@@ -0,0 +1,19 @@
+package com.tutorial.main;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+	private static final int width = 16, height = 16, biggerwidth =64, biggerheight = 64,nWidth=12,nHeight=20;
+	
+	
+	public static BufferedImage car;
+								
+	public static void init(){
+		
+	//	SpriteSheet cars = new SpriteSheet(ImageLoader.loadimage("textures/cars.png"));
+
+		
+		//car = cars.crop(0, 0, width, height);
+	}
+	
+}
