db5861eeaa5a60c096ca03650418d9f7e2b52cc6
Viktor Lindblad <Viktor Lindblad@DESKTOP-NK4S0QF>
2018-10-22 20:46:21 +0200

    fixed fps

--- The Game/src/com/tutorial/main/Game.java
+++ The Game/src/com/tutorial/main/Game.java
@@ -43,23 +43,25 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void run() {
+		
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
 		double delta = 0;
 		long timer = System.currentTimeMillis();
 		int frames = 0;
+		
 		while(running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while(delta >= 1){
+			if(delta >= 1){
 				tick();
-				delta--;
-			}
-			if(running)
 				render();
-			frames++;
+				delta--;
+				frames++;
+			}
+
 			
 			if(System.currentTimeMillis() - timer > 1000){
 				timer += 1000;
