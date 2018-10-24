408d19eb78bb74304eced5bfb2e7c3995d5a957e
Viktor Lindblad <Viktor Lindblad@DESKTOP-NK4S0QF>
2018-10-24 21:15:01 +0200

    Fixade lite saker

--- The Game/src/com/tutorial/main/Player.java
+++ The Game/src/com/tutorial/main/Player.java
@@ -1,18 +1,17 @@
 package com.tutorial.main;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
+
 
 public class Player {
 
 	private int x,y,width,height;
 	private float gravity,velocity;
 	private Rectangle hitbox;
-	private BufferedImage[] runAnimation;
-	private BufferedImage[] jumpAnimation;
+
+
 	private KeyManager k;
 	private Game game;
 	private boolean gameOver;
@@ -20,8 +19,8 @@ public class Player {
 	public Player(int x, int y,KeyManager k,Game game){
 		this.width = game.getRect().width/12;
 		this.height = game.getRect().height/12;
-		width*=2;
-		height*=2;
+//		width*=2;
+//		height*=2;
 		this.x = x-width/2;
 		this.y = y-height/2;
 		
@@ -30,13 +29,6 @@ public class Player {
 		gravity = 0.9f;
 		gameOver = false;
 		this.game = game;
-		runAnimation = new BufferedImage[4];
-		jumpAnimation = new BufferedImage[4];
-		
-		runAnimation[0] = Assets.run1;
-		runAnimation[1] = Assets.run2;
-		runAnimation[2] = Assets.run3;
-		runAnimation[3] = Assets.run4;
 	}
 	
 	
@@ -77,19 +69,7 @@ public class Player {
 	}
 	
 	public void render(Graphics g){
-		
 		g.drawImage(Assets.oscar, x, y,width,height ,null);
-		//g.fillRect(hitbox.x,hitbox.y,hitbox.width,hitbox.height);
-
-		/*if(game.getFrames()<=15){
-			g.drawImage(Assets.run1, x,y,64,128,null);
-		} else if(game.getFrames()<=30&&game.getFrames()>15){
-			g.drawImage(Assets.run2, x,y,64,128,null);
-		} else if(game.getFrames()<=45&&game.getFrames()>30){
-			g.drawImage(Assets.run3, x,y,64,128,null);
-		} else if(game.getFrames()<=60&&game.getFrames()>45){
-			g.drawImage(Assets.run4, x,y,64,128,null);
-		}*/
 	}
 	
 }
--- The Game/src/com/tutorial/main/World.java
+++ The Game/src/com/tutorial/main/World.java
@@ -10,9 +10,8 @@ import java.util.Stack;
 public class World {
 
 	private int  height,width,divider,speed;
-	private ArrayList<Rectangle> blocks;
 	private ArrayList<Rectangle> filledBlocks,passedBlocks;
-	private Stack<Rectangle> removeQueue,removeFill,removePass;
+	private Stack<Rectangle> removeFill,removePass;
 	private Game game;
 	private float points;
 	
@@ -20,23 +19,17 @@ public class World {
 		divider = 12;
 		this.width = width;
 		this.height = height;
-		removeQueue = new Stack<Rectangle>();
 		removeFill = new Stack<Rectangle>();
 		removePass = new Stack<Rectangle>();
 		points = 0;
 		filledBlocks = new ArrayList<Rectangle>();
 		passedBlocks = new ArrayList<Rectangle>();
-		blocks = new ArrayList<Rectangle>();
-		speed=4;
+
+		speed = 4;
 		this.game = game;
 		
 		nextBlock();
-		/*
-		for(int x=0; x<width+(2*(width/10)); x+=(width/divider)){
-			for(int y=0; y<height; y+=(height/divider)){
-				blocks.add(new Rectangle(x,y,width/divider,height/divider));
-			}
-		}*/
+
 	}
 	
 	public float getPoints(){
@@ -53,9 +46,9 @@ public class World {
 	
 	private void nextBlock(){
 		Random random = new Random();
-		int r = (random.nextInt(4 - 2 + 2)+2);
+		int r = (random.nextInt(3 - 2 + 2)+2);
 		filledBlocks.add(new Rectangle(width,height-(height/divider)*r,width/divider,r*height/divider));
-		filledBlocks.add(new Rectangle(width,0,width/divider,(random.nextInt(4 - 2 + 2)+2)*height/divider));
+		filledBlocks.add(new Rectangle(width,0,width/divider,(random.nextInt(3 - 2 + 2)+2)*height/divider));
 	}
 	
 	public void tick(){
@@ -63,19 +56,19 @@ public class World {
 			return;
 		}
 		int lowestX=-(width/divider);
-
-/*		for(Rectangle temp:blocks){
-			
-			if(temp.x<lowestX){
-				removeQueue.push(temp);
-			}
-			temp.setLocation((int)temp.getX()-2,temp.y);
-			
-		}
-	*/	
 		
 		int lowerX=width/2;
 		
+		for(Rectangle temp:passedBlocks){
+			if(temp.x<lowestX){
+				removePass.add(temp);
+			}
+			temp.setLocation((int)temp.getX()-speed,temp.y);
+		}
+		while(!removePass.isEmpty()){
+			passedBlocks.remove(removePass.pop());
+			
+		}
 		for(Rectangle temp:filledBlocks){
 			if(temp.x<lowerX){
 				removeFill.push(temp);
@@ -95,33 +88,13 @@ public class World {
 		if(nextBlock){
 			nextBlock();
 		}
+
 		
-		for(Rectangle temp:passedBlocks){
-			if(temp.x<lowestX){
-				removePass.add(temp);
-			}
-			temp.setLocation((int)temp.getX()-speed,temp.y);
-		}
-		while(!removePass.isEmpty()){
-			passedBlocks.remove(removePass.pop());
-			
-		}
-		int y=0;
-		/*
-		while(!removeQueue.isEmpty()){
-			blocks.remove(removeQueue.pop());
-			blocks.add(new Rectangle(width+((width/divider)),y,width/divider,height/divider));
-			y+=(height/divider);
-		}
-		*/
+
 	}
 	
 	public void render(Graphics g){
-		/*
-		for(Rectangle temp:blocks){
-			g.setColor(Color.black);
-			g.drawRect(temp.x, temp.y, temp.width, temp.height);
-		}*/
+
 		for(Rectangle temp:filledBlocks){
 			g.setColor(Color.black);
 			g.fillRect(temp.x, temp.y, temp.width, temp.height);
